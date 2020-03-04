026887e2d57a86fa73e67d04938c82f30ba92c92
Nedas <vvinickas@gmail.com>
2020-03-04 23:43:13 +0200

    MainMenu ready

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        //do something
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TempScene");
+    }
+}
