3e9df5b7eb0edf9fcfd989708182f6cf6d801bdc
Pikezio <Pauliuskuliesiuskl@gmail.com>
2020-03-10 18:51:37 +0200

    Basic transition implementation GAM2

---
+++ Assets/Scripts/TrainController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TrainController : MonoBehaviour
+{
+    public Transform targetBeginning;
+    public Transform targetEnding;
+    public Transform trigger;
+    public float speed = 10f;
+
+    public bool endLevel;
+
+    private void Start()
+    {
+        endLevel = false;
+    }
+
+    private void Update()
+    {
+        if (!endLevel)
+        {
+            MoveTo(targetBeginning);
+        }
+
+        if (endLevel)
+        {
+            MoveTo(targetEnding);
+        }
+    }
+
+    //Moves to a target at a certain speed
+    public void MoveTo(Transform target)
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > 0.001f)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    //Makes train drive away and changes to specified level
+    public void ChangeLevel(string nextScene)
+    {
+        //Here we will be able to check if we have everything to proceed.
+        StartCoroutine(EndLevel(nextScene));
+    }
+
+    IEnumerator EndLevel(string nextScene)
+    {
+        yield return new WaitForSeconds(1);
+        endLevel = true;
+        yield return new WaitForSeconds(8);
+        SceneManager.LoadScene(nextScene);
+    }
+}
---
+++ Assets/TriggerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    public GameObject train;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            train.GetComponent<TrainController>().ChangeLevel("TempScene");
+        }
+    }
+}
