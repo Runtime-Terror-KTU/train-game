87bd1ac94f08f48731fe6f9d42c52d743e1e9873
Nedas <vvinickas@gmail.com>
2020-04-01 20:05:30 +0300

    HUD2 Player health
    -Player's health is displayed on the screen

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -6,24 +6,24 @@ public class Interaction : MonoBehaviour
     public GameObject interactableObj;
     public LayerMask LayerInteract;
 
-    // void Update()
-    //{
-    //    RaycastHit hit;
-    //    Vector3 fwd = transform.TransformDirection(Vector3.forward);       
-    //    if(Physics.Raycast(transform.position,fwd,out hit,radius,LayerInteract.value))
-    //    {
-    //        if(hit.collider.CompareTag("InteractableObj"))
-    //        {
-    //            interactableObj = hit.collider.gameObject;
+    public void Interact()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, out hit, radius, LayerInteract.value))
+        {
+            if (hit.collider.CompareTag("InteractableObj"))
+            {
+                interactableObj = hit.collider.gameObject;
 
-    //            if (Input.GetKeyDown("e"))
-    //            {
-    //                Debug.Log("Succes");
-    //                interactableObj.SetActive(false);
-    //            }
-                
-    //        }
-    //    }
-    //}
-    
+                if (Input.GetKeyDown("e"))
+                {
+                    Debug.Log("Succes");
+                    interactableObj.SetActive(false);
+                }
+
+            }
+        }
+    }
+
 }
