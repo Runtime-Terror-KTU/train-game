0f8ee3bdf11cab2737c4785e5afc23af2650191f
vizorian <birstonasvilius@gmail.com>
2020-04-26 10:35:09 +0300

    Complete control overhaul

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -15,19 +15,31 @@ public class PlayerControls : MonoBehaviour
     
     bool walking, jump;
     public bool isGrounded { get; private set; }
+    public bool hasJumpedThisFrame { get; private set; }
     bool isJumping;
     Vector3 jumpDirection;
     Vector3 groundNormal;
     Vector3 characterVelocity;
 
+    LayerMask groundCheckLayers = -1;
 
     public Animator animator;
     public float movSpeed = 6f;
     public float sideSpeed = 5f;
     public float backSpeed = 3f;
+    public float LastTimeJumped = 0f;
+
     public bool isFiring;
     public bool isReloading;
 
+    public float gravityForce = 20f;
+    public float maxSpeedGrounded = 10f;
+    public float maxSpeedAir = 10f;
+    public float movementAcceleration = 15;
+    public float airAcceleration = 20f;
+    public float jumpForce = 5f;
+    const float JumpPreventionTime = 0.2f;
+    const float GroundCheckDist = 0.07f;
     // Start is called before the first frame update
     void Start()
     {
@@ -56,44 +68,114 @@ public class PlayerControls : MonoBehaviour
     void Movement()
     {
 
-        if (walking)
+
+
+        //if (walking)
+        //{
+        //    movSpeed = 3f;
+        //    sideSpeed = 3f;
+        //    backSpeed = 3f;
+        //}
+        //else
+        //{
+        //    movSpeed = 6;
+        //    sideSpeed = 5f;
+        //    backSpeed = 3f;
+        //}
+
+        Vector3 inputsNormalized = new Vector3(inputs.x, 0, inputs.z);
+        inputsNormalized.Normalize();
+
+        Vector3 worldMoveInput = transform.TransformVector(inputsNormalized);
+
+        //ground movement
+        if (isGrounded)
         {
-            movSpeed = 3f;
-            sideSpeed = 3f;
-            backSpeed = 3f;
+            Vector3 targetVelocity = worldMoveInput * maxSpeedGrounded;
+
+            //if crouched?
+            //smooth interpolation
+            characterVelocity = Vector3.Lerp(characterVelocity, targetVelocity, movementAcceleration * Time.deltaTime);
+
+            if (isGrounded && inputsNormalized.y > 0)
+            {
+                characterVelocity = new Vector3(characterVelocity.x, 0f, characterVelocity.z);
+                characterVelocity += Vector3.up * jumpForce;
+
+                //sound for jump
+
+                //track time
+                LastTimeJumped = Time.time;
+                hasJumpedThisFrame = true;
+
+                isGrounded = false;
+                groundNormal = Vector3.up;
+            }
+            //footstep frequency, distance & sound
         }
+        //air movement
         else
         {
-            movSpeed = 6;
-            sideSpeed = 5f;
-            backSpeed = 3f;
+            characterVelocity += worldMoveInput * airAcceleration * Time.deltaTime;
+
+            float verticalVelocity = characterVelocity.y;
+            Vector3 horizontalVelocity = Vector3.ProjectOnPlane(characterVelocity, Vector3.up);
+            horizontalVelocity = Vector3.ClampMagnitude(horizontalVelocity, maxSpeedAir);
+            characterVelocity = horizontalVelocity + (Vector3.up * verticalVelocity);
+
+            //gravity
+            characterVelocity += Vector3.down * gravityForce * Time.deltaTime;
         }
 
-        
-        Vector3 inputNormalized = new Vector3(inputs.x, 0, inputs.z);
-        inputNormalized.Normalize();
-        float test = Mathf.Abs(inputs.x) + Mathf.Abs(inputs.z);
-        animator.SetFloat("Movement", test);
+        //apply velocity
+        Vector3 capsuleBotBeforeMove = GetCapsuleBot();
+        Vector3 capsuleTopBeforeMove = GetCapsuleTop(controller.height);
+        controller.Move(characterVelocity * Time.deltaTime);
 
-        //Vector3 move = new Vector3(inputNormalized.x, 0f, inputNormalized.z);
-        //Vector3 worldInput = transform.TransformVector(move);
-
-        //controller.SimpleMove(inputNormalized * movSpeed);
-        float vertInput = Input.GetAxis("Vertical");
-        float horizInput = Input.GetAxis("Horizontal");
-
-        Vector3 forwardMovement = transform.forward * vertInput;
-        Vector3 sideMovement = transform.right * horizInput;
-
-        controller.SimpleMove(Vector3.ClampMagnitude(forwardMovement + sideMovement, 1.0f) * movSpeed);
-
-        
+        if (Physics.CapsuleCast(capsuleBotBeforeMove, capsuleTopBeforeMove, controller.radius, characterVelocity.normalized, out RaycastHit hit, characterVelocity.magnitude * Time.deltaTime, -1, QueryTriggerInteraction.Ignore))
+            characterVelocity = Vector3.ProjectOnPlane(characterVelocity, hit.normal);
     }
 
+    void GroundCheck()
+    {
+        isGrounded = false;
+        groundNormal = Vector3.up;
+
+        if (Time.time >= LastTimeJumped + JumpPreventionTime)
+            if(Physics.CapsuleCast(GetCapsuleBot(), GetCapsuleTop(controller.height), controller.radius, Vector3.down, out RaycastHit hit, GroundCheckDist, groundCheckLayers, QueryTriggerInteraction.Ignore))
+            {
+                groundNormal = hit.normal;
+
+                if(Vector3.Dot(hit.normal, transform.up) > 0f && IsNormalUnderSlopeLimit(groundNormal))
+                {
+                    isGrounded = true;
+                    if(hit.distance > controller.skinWidth)
+                        controller.Move(Vector3.down * hit.distance);
+                }
+            }
+    }
+
+    Vector3 GetCapsuleBot()
+    {
+        return transform.position + (transform.up * controller.radius);
+    }
+
+    Vector3 GetCapsuleTop(float atH)
+    {
+        return transform.position + (transform.up * (atH - controller.radius));
+    }
+
+    bool IsNormalUnderSlopeLimit(Vector3 normal)
+    {
+        return Vector3.Angle(transform.up, normal) <= controller.slopeLimit;
+    }
 
     void GetInputs()
     {
-        jump = Input.GetKey(controls.jump);
+        if (Input.GetKey(controls.jump))
+            inputs.y = 1;
+        else inputs.y = 0;
+
 
         if (Input.GetKey(controls.walk))
             walking = true;
