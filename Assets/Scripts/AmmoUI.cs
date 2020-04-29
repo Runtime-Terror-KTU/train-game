c2a2fd2ae22037a7c99065f307975221de5adffe
Nedas <vvinickas@gmail.com>
2020-04-30 00:50:05 +0300

    UI upgrade
    -Color of health changes(depends on ammount)
    -Melee weapon does not show ammo

--- Assets/Scripts/AmmoUI.cs
+++ Assets/Scripts/AmmoUI.cs
@@ -8,16 +8,20 @@ public class AmmoUI : MonoBehaviour
     public Text currentAmmoText;
     public Text reserveAmmoText;
     public WeaponSystem weaponSystem;
+    public Image ammoPanel;
+
     // Update is called once per frame
     void Update()
     {
         if (weaponSystem.selectedWeapon == 0)
         {
+            ammoPanel.enabled = false;
             currentAmmoText.text = "";
             reserveAmmoText.text = "";
         }
         else
         {
+            ammoPanel.enabled = true;
             currentAmmoText.text = weaponSystem.currentAmmo.ToString();
             reserveAmmoText.text = weaponSystem.currentReserve.ToString();
         }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -16,10 +16,25 @@ public class PlayerHealth : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if(player.Health>=70)
+        {
+            healthText.color = Color.green;
+        }
+        else if (player.Health<70&&player.Health>=30)
+        {
+            healthText.color = Color.yellow;
+        }
+        else if(player.Health<30)
+        {
+            healthText.color = Color.red;
+        }
+
         if(health > player.Health || health < player.Health)
         {
             anim.Play();
         }
+
+
 
         healthText.text = player.Health.ToString();
         health = player.Health;
