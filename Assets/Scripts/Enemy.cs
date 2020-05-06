ca0ee840c5424f4d6fb71523df607b930ab91fce
justas-budginas <jusbud2@ktu.edu>
2020-05-06 22:13:10 +0300

    Added ragdoll physics to the enemy
    When enemy dies, it turns in to ragdoll

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,11 +11,13 @@ public class Enemy : MonoBehaviour
     public float MovementSpeed = 4f;
     public float MeleeDamage = 25f;
     public float MeleeSpeed = 0.5f;
+    public float DieTime = 3f;
 
 
     void Start()
     {
-        
+        setRigidbodyState(true);
+        setColliderState(false);
     }
 
     void Update()
@@ -28,13 +31,47 @@ public class Enemy : MonoBehaviour
 
         if(Health <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
     }
 
     public void TakeDamage(float dmg)
     {
         Health -= dmg;
+    }
+
+    void Die()
+    {
+        GetComponent<Animator>().enabled = false;
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<EnemyAI>().enabled = false;
+        setRigidbodyState(false);
+        setColliderState(true);
+        Destroy(gameObject, DieTime);
+    }
+
+    void setRigidbodyState(bool state)
+    {
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody rb in rigidbodies)
+        {
+            rb.isKinematic = state;
+        }
+
+        GetComponent<Rigidbody>().isKinematic = !state;
+    }
+
+    void setColliderState(bool state)
+    {
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach(Collider cs in colliders)
+        {
+            cs.enabled = state;
+        }
+
+        GetComponent<Collider>().enabled = !state;
     }
 
     private void OnCollisionEnter(Collision collision)
