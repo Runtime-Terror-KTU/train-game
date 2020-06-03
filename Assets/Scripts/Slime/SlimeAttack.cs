b9f7333b7a8c612b313fc0c4a984b07e40bb42c8
Pikezio <Pauliuskuliesiuskl@gmail.com>
2020-06-03 15:23:58 +0300

    Slime final

--- Assets/Scripts/Slime/SlimeAttack.cs
+++ Assets/Scripts/Slime/SlimeAttack.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class SlimeAttack : MonoBehaviour
 {
     Player player;
-    public float Health = 50;
-    public float DieTime = 1.1f;
+    public Animator anim;
+    public float Health = 30;
+    public float DieTime = 5f;
     // Start is called before the first frame update
+
+    bool dead = false;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        anim = this.GetComponent<Animator>();
     }
 
     void Update()
     {
         if (Health <= 0)
         {
-            FindObjectOfType<AudioManager>().Play("Death");
-            FindObjectOfType<Animator>().SetTrigger("Death");
-            Destroy(this.gameObject, DieTime);
+            if (dead != true)
+            {
+                dead = true;
+                StartCoroutine(Die());
+            }
         }
     }
 
@@ -31,5 +37,13 @@ public class SlimeAttack : MonoBehaviour
     public void TakeDamage(float dmg)
     {
         Health -= dmg;
+    }
+
+    IEnumerator Die()
+    {
+        FindObjectOfType<AudioManager>().Play("Death");
+        anim.SetTrigger("Death");
+        yield return new WaitForSeconds(3); 
+        Destroy(gameObject);
     }
 }
