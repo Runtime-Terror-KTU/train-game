b68e705a190e8428e4319a024b395c45ee878c2c
raqua41 <aurimasvarnelis@gmail.com>
2020-06-03 22:12:57 +0300

    Enemy count UI
    Enemy count UI

---
+++ Assets/Scripts/EnemyCountUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCountUI : MonoBehaviour
+{
+    public Text enemyCountText;
+   
+    void Update()
+    {
+        enemyCountText.text = (GameObject.FindGameObjectsWithTag("Enemy").Length).ToString();
+    }
+
+}
